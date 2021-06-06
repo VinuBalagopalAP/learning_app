04c4282ccf75203e4edce473b7fa5917ea7b5751
VinuBalagopalAP <vinubalagopalap@gmail.com>
2021-06-07 02:45:13 +0530

    onboarding screen added

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,23 @@
 import 'package:flutter/material.dart';
-import 'package:shopping_app/screens/homepage.dart';
+import 'package:flutter/services.dart';
+import 'package:shopping_app/screens/onboarding.dart';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    SystemChrome.setSystemUIOverlayStyle(
+      SystemUiOverlayStyle.dark.copyWith(statusBarColor: Colors.transparent),
+    );
+
     return MaterialApp(
-      home: HomePage(),
+      title: 'Introduction screen',
       debugShowCheckedModeBanner: false,
+      theme: ThemeData(primarySwatch: Colors.blue),
+      home: SafeArea(
+        child: OnBoardingPage(),
+      ),
     );
   }
 }
--- lib/screens/homepage.dart
+++ lib/screens/homepage.dart
@@ -1,17 +1,11 @@
 import 'package:flutter/material.dart';
-import 'package:hexcolor/hexcolor.dart';
 
 class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Container(
-          color: HexColor(
-            "#53B175",
-          ),
-        ),
-      ),
+      appBar: AppBar(title: const Text('Home')),
+      body: const Center(child: Text("This is the screen after Introduction")),
     );
   }
 }
